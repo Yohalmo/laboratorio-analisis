78b3c4b1ab2975c3a7603d75de329b5ccec59d58
YeimiG <garciaaldanayeimisucely@gmail.com>
2025-05-16 10:42:28 -0600

    me faltaba algo
    ...

--- cliente-react/page/main/dashboard.tsx
+++ cliente-react/page/main/dashboard.tsx
@@ -1,15 +1,38 @@
-import { useContext, useState } from "react";
-import { GoogleLogin } from "@react-oauth/google";
-import { AuthContext } from '../../context/AuthContext';
-import  Alumnos  from './alumnos';
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getDashboardStats, getLastAlumnos } from "../../api/dashboard";
+import CardResumen from "../../src/components/cardResumen";
+import TablaResumen from "../../src/components/alumnosResumen";
+import type { DashboardStats, Alumno } from "../../src/types";
 
-export default function Dashboard(){
-    const { login } = useContext(AuthContext);
+export default function Dashboard() {
+    const [stats, setStats] = useState<DashboardStats | null>(null);
+    const [alumnos, setAlumnos] = useState<Alumno[]>([]);
+  
+
+    useEffect(() => {
+        // Tipado explícito en las respuestas
+        getDashboardStats().then((data: DashboardStats) => setStats(data));
+        getLastAlumnos().then((data: Alumno[]) => setAlumnos(data));
+    
+    }, []);
 
     return (
-        <div>
-            funciona
+        <div className="container py-5">
+            <h1 className="mb-4 text-center text-primary">Dashboard General</h1>
+
+            {/* Cards de Resumen */}
+            <div className="row g-4 mb-4">
+                <CardResumen title="Alumnos" count={stats?.alumnos || 0} color="primary" />
+              
+            </div>
+
+            {/* Tablas de Últimos Registros */}
+            <div className="mb-5">
+                <h3 className="mb-3">Últimos Alumnos</h3>
+                <TablaResumen data={alumnos} type="alumnos" />
+            </div>
+
+            
         </div>
-    )
+    );
 }
